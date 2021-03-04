1975a58b87494502d6eb0a6a9f211f46e0ad26c6
Valsorim98 <80041450+Valsorim98@users.noreply.github.com>
2021-03-04 15:01:40 +0200

    Add files via upload

---
+++ number.py
@@ -0,0 +1,8 @@
+num = int(input())
+
+if num > 0:
+    print('The number ' + str(num) + ' is higher than 0')
+elif num < 0:
+        print('The number ' + str(num) + ' is lower than 0')
+elif num == 0:
+        print('The number equals 0')
